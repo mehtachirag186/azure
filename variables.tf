ab3e671adcda76b54bc0dca6c62189ad826b3640
mehtachirag186 <97532641+mehtachirag186@users.noreply.github.com>
2022-05-12 17:00:25 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,27 @@
+/* variable "aws_vpc" {
+  default = "default"
+
+} */
+variable "region" {
+  default = "us-east-1"
+}
+variable "resource_group" {
+  default = "rg-spservers"
+}
+
+/* variable "ami" {
+  default = "ami-08ed5c5dd62794ec0"
+  #type = map
+} */
+
+/* variable "Instance" {
+  default = "t2.micro"
+  #type = string
+} */
+
+variable "ServerName" {
+  default = "SP2k19"
+}
+variable "loadbalancer" {
+  default = "sp2019elb"
+}
