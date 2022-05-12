d144d36953ed9629141149d9bd8ab528c74e2f84
mehtachirag186 <97532641+mehtachirag186@users.noreply.github.com>
2022-05-12 18:40:46 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,6 +1,19 @@
 resource "azurerm_resource_group" "SPservers" {
   name = var.resource_group
   location = var.region
-
  
+}
+
+resource "azurerm_resource_group" "Vnet-SpServers" {
+  name = var.resource_group
+  location = var.region
+ 
+}
+
+resource "azurerm_virtual_network" "SP-Virtual-Network" {
+  name = var.Vnet
+  location = var.region
+  resource_group_name = var.resource_group
+  address_space = ["10.0.0.0/16"]
+    
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,3 @@
-/* variable "aws_vpc" {
-  default = "default"
-
-} */
 variable "region" {
   default = "East US"
 }
@@ -9,16 +5,10 @@ variable "resource_group" {
   default = "rg-spservers"
 }
 
-/* variable "ami" {
-  default = "ami-08ed5c5dd62794ec0"
-  #type = map
-} */
-
-/* variable "Instance" {
-  default = "t2.micro"
-  #type = string
-} */
-
+variable "Vnet" {
+  default = "SP-Virtual-Network"
+  
+}
 variable "ServerName" {
   default = "SP2k19"
 }
