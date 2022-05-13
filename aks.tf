e2023a4c23ae605289db35bccd4bd46a740205c6
mehtachirag186 <97532641+mehtachirag186@users.noreply.github.com>
2022-05-13 12:21:45 +0530

    Update aks.tf

--- aks.tf
+++ aks.tf
@@ -1,16 +1,16 @@
-resource "azurerm_kubernetes_cluster" "aks" {
-    name = var.aks
-    location = var.region
-    resource_group_name = var.aks
-    dns_prefix = "testaks"
-    default_node_pool {
-      name = "default"
-      node_count = 1
-      vm_size = "Standard_D2_V2"
-    }
+# resource "azurerm_kubernetes_cluster" "aks" {
+#     name = var.aks
+#     location = var.region
+#     resource_group_name = var.aks
+#     dns_prefix = "testaks"
+#     default_node_pool {
+#       name = "default"
+#       node_count = 1
+#       vm_size = "Standard_D2_V2"
+#     }
 
-    identity {
-      type = SystemAssigned
-    }
+#     identity {
+#       type = SystemAssigned
+#     }
   
-}
+# }
